cfe6a97305763d591039a1d2a5e72e74190d0de1
difint <dev@difint.co>
2020-08-26 08:19:10 -0700

    CALC-2: Use ANTLR instead of DataTable

--- BlazorCalcTest/UnitTest1.cs
+++ BlazorCalcTest/UnitTest1.cs
@@ -17,5 +17,15 @@ namespace BlazorCalcTest
             Assert.AreEqual(new Calculator("", "423", "+").calculate(), "423");
             Assert.AreEqual(new Calculator("foo", "14", "+").calculate(), "14");
         }
+        [TestMethod]
+        public void TestMultipleOperationsAndPrecedence()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(calc.evaluate("42+42*42-42/42"), "1805");
+            Assert.AreEqual(calc.evaluate("1+2*3/4+5"), "7.5");
+            Assert.AreEqual(calc.evaluate("0.5*1.5/0.25-1"), "2");
+            Assert.AreEqual(calc.evaluate("0.025+12.5*15.25+2+3.5/10"), "193");
+        }
     }
 }
+
--- README.md
+++ README.md
@@ -12,4 +12,6 @@
 * https://docs.microsoft.com/en-us/aspnet/core/blazor/forms-validation?view=aspnetcore-3.1
 * https://docs.microsoft.com/en-us/dotnet/architecture/blazor-for-web-forms-developers/components
 * https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=netcore-3.1
-
+* https://stackoverflow.com/questions/23092081/antlr4-visitor-pattern-on-simple-arithmetic-example
+* https://stackoverflow.com/questions/8590402/antlr-for-c-sharp-and-clscompliant-attribute
+* https://github.com/mlindegarde/blog--antlr4-dotnet-core/blob/master/visitor/src/Calculator/Parsing/CalculatorVisitor.cs
